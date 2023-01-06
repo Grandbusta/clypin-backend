111050bfe92000827cd6de917989f3729a5dcbe1
Grandbusta <bustajay30@gmail.com>
2023-01-06 19:31:12 +0100

    update

--- models/user.go
+++ models/user.go
@@ -5,12 +5,12 @@ import (
 )
 
 type User struct {
-	ID        uint64    `gorm:"primaryKey;autoIncrement" json:"id"`
-	Email     string    `gorm:"not null" json:"email"`
-	FirstName string    `gorm:"not null" json:"first_name"`
-	LastName  string    `gorm:"not null" json:"last_name"`
-	Password  string    `gorm:"not null" json:"password"`
-	Messages  []Message `json:"messages"`
+	ID        uint64 `gorm:"primaryKey;autoIncrement" json:"id"`
+	Email     string `gorm:"not null" json:"email"`
+	FirstName string `gorm:"not null" json:"first_name"`
+	LastName  string `gorm:"not null" json:"last_name"`
+	Password  string `gorm:"not null" json:"password"`
+	Messages  []Message
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
